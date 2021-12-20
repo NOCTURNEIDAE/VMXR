5b06f1f8c73497ab7412b293460825bf40438fdf
NOCTURNE <nathanroy1@gmail.com>
2021-12-19 23:53:29 -0500

    dev-viz

---
+++ _ext/_CtrlViz2Ext.py
@@ -0,0 +1,11 @@
+
+from TDStoreTools import StorageManager
+import TDFunctions as TDF
+
+class CtrlViz2Ext:
+
+	def __init__(self, ownerComp):
+		self.ownerComp = ownerComp
+
+		TDF.createProperty(self, 'Index', value=1, dependable=True,
+						   readOnly=False)
---
+++ _ext/_CtrlViz3Ext.py
@@ -0,0 +1,11 @@
+
+from TDStoreTools import StorageManager
+import TDFunctions as TDF
+
+class CtrlViz3Ext:
+
+	def __init__(self, ownerComp):
+		self.ownerComp = ownerComp
+
+		TDF.createProperty(self, 'Index', value=1, dependable=True,
+						   readOnly=False)
